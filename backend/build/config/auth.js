b370576b9cfa7ceceb36a1047039cfec3ff3c82e
Luiz Victor  Nascimento Andrade <lvictor@digiboard.com.br>
2023-01-12 09:59:42 -0400

    babel

--- backend/build/config/auth.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    jwt: {
-        secretKey: process.env.APP_SECRET,
-        expiresIn: '7d'
-    }
-};
